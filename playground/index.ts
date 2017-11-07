7fa8b03034949a5efbac0ce7fc36a551a892c891
Matteo Joliveau <m.joliveau@koor.it>
2017-11-07 11:30:57 +0100

    Configurable colors now supported Reintroduced Playground for easy playtesting during development

--- playground/index.ts
+++ playground/index.ts
@@ -1,35 +1,47 @@
 /**
  * This is only for local test
  */
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { Component } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {Component} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { VerticalTimelineModule } from 'angular-vertical-timeline';
+import {VerticalTimelineModule} from 'angular-vertical-timeline';
 
 @Component({
-  selector: 'app',
-  template: `<vertical-timeline>
-  
-  <vertical-timeline-card>
-   <h1>Today</h1>
-  </vertical-timeline-card>
+    selector: 'app-component',
+    template: `
+        <vertical-timeline>
 
-  <vertical-timeline-card>
-    <h1>Tomorrow</h1>
-  </vertical-timeline-card>
-  
-</vertical-timeline>
-`
+            <vertical-timeline-card [color]="'#08D198'">
+                <h1>Today</h1>
+            </vertical-timeline-card>
+
+            <vertical-timeline-card [color]="'#331111'">
+                <h1>Tomorrow</h1>
+            </vertical-timeline-card> 
+            
+            <vertical-timeline-card [color]="'#08D198'">
+                <h1>Today</h1>
+            </vertical-timeline-card>
+
+            <vertical-timeline-card [color]="'#331111'">
+                <h1>Tomorrow</h1>
+            </vertical-timeline-card>
+
+        </vertical-timeline>
+    `,
+    styleUrls: ['styles.css']
 })
-class AppComponent { }
+class AppComponent {
+}
 
 @NgModule({
-  bootstrap: [AppComponent],
-  declarations: [AppComponent],
-  imports: [BrowserModule, VerticalTimelineModule]
+    bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, VerticalTimelineModule]
 })
-class AppModule { }
+class AppModule {
+}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
