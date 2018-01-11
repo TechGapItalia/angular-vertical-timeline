05c5fbd950b1ebc8da0e08bbf61d320426a78308
Matteo Joliveau <m.joliveau@koor.it>
2018-01-11 11:02:22 +0100

    Fixed text in mobile inside vertical-card

--- playground/esm/src/app/app.component.ts
+++ playground/esm/src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<vertical-timeline><vertical-timeline-card>Hello!</vertical-timeline-card><vertical-timeline-card>World!</vertical-timeline-card></vertical-timeline>`
+  template: `
+    <vertical-timeline>
+      <vertical-timeline-card>Spicy jalapeno bacon ipsum dolor amet tenderloin buffalo chuck pancetta pork belly
+        landjaeger tail frankfurter swine porchetta ribeye. Cow turducken shoulder pastrami chicken. T-bone doner beef,
+        kielbasa buffalo burgdoggen strip steak fatback filet mignon pork belly picanha pig chuck. Chuck boudin jowl
+        tail meatloaf. Tri-tip prosciutto hamburger meatloaf biltong shank kevin. Kevin rump picanha pastrami, cupim
+        turkey shank beef ribs short ribs ground round hamburger. Ball tip burgdoggen strip steak porchetta landjaeger.
+      </vertical-timeline-card>
+      <vertical-timeline-card>
+        Beef ribs andouille shank, tri-tip ribeye cow salami jerky tail bacon porchetta. Fatback
+        buffalo ribeye, prosciutto kielbasa pork ball tip sausage rump drumstick. Short loin strip steak salami, alcatra
+        cupim turkey buffalo meatloaf bresaola chicken tri-tip picanha. Pancetta kevin frankfurter cow ham drumstick
+        rump tongue sausage pork fatback picanha t-bone short loin. Chicken hamburger sausage swine corned beef flank
+        turducken chuck prosciutto beef tongue spare ribs fatback ribeye.
+      </vertical-timeline-card>
+    </vertical-timeline>`
 })
 export class AppComponent {
   public header: string = 'UMD Demo';
